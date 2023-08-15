9494b91b1572de8302ccf99a05ed2bac118e0f79
haydercharfeddin <haydercharfeddin51@gmail.com>
2023-08-15 17:21:19 +0100

    init

---
+++ backEnd/mongoDb/Product.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const productSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    imageUrl: String,
+    price:Number,
+    category: {
+      type: String,
+      enum: ["SUV", "SUPERCAR", "CLASS"],
+      default: "CLASS"
+    }
+  });
+const userSchema = new mongoose.Schema ({
+  username:String,
+  email :String,
+  password:String
+})
+const Users =mongoose.model("Users",userSchema) ;
+const Product = mongoose.model("Product", productSchema);
+module.exports = {Product,Users}
+;
